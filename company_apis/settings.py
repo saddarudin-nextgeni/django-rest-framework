20213ae9a6c5f8703d0087e712bd93cd59d747b5
Saddar U Din <saddarudin@nextgeni.com>
2026-05-18 18:25:23 +0500

    create api app and added to settings file

--- company_apis/settings.py
+++ company_apis/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+}
 
 ROOT_URLCONF = 'company_apis.urls'
 
--- company_apis/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.decorators import api_view
-from django.http import HttpResponse
-
-
-@api_view(['GET'])
-def home(request):
-    return HttpResponse("Welcome to the Company APIs Home Page!")
