4839dafdfcc1c2a474712f286da15c9ecc5e15cd
Saddar U Din <saddarudin@nextgeni.com>
2026-05-19 14:46:52 +0500

    register company and employee model in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Company, Employee
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('company_id', 'name', 'location', 'industry')
+    search_fields = ('name', 'location', 'industry')
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('employee_id', 'name', 'position', 'company')
+    search_fields = ('name', 'position')
+    list_filter = ('company',)
