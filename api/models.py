7426d4cae8a673914edf36620e05504b62d936e0
Saddar U Din <saddarudin@nextgeni.com>
2026-05-19 12:30:47 +0500

    create employee model

--- api/models.py
+++ api/models.py
@@ -8,4 +8,12 @@ class Company(models.Model):
     location = models.CharField(max_length=255)
     industry = models.CharField(max_length=255)
     about = models.TextField()
-    
+
+
+class Employee(models.Model):
+    employee_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    position = models.CharField(max_length=255)
+    company = models.ForeignKey(Company, related_name='employees', on_delete=models.CASCADE)
