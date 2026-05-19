1c78547ae3b01e1c65a8de32ce85aee35c768919
Saddar U Din <saddarudin@nextgeni.com>
2026-05-19 14:23:53 +0500

    created api for getting employees of a particular company

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Company, Employee
 from .serializer import CompanySerializer, EmployeeSerializer
 
@@ -7,6 +9,13 @@ from .serializer import CompanySerializer, EmployeeSerializer
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+    @action(detail=True, methods=['get'])
+    def employees(self, request, pk=None):
+        company = self.get_object()
+        employees = company.employees.all()
+        serializer = EmployeeSerializer(employees, many=True, context={'request': request})
+        return Response(serializer.data)
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
