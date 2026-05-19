92b25337c4abd15791abf9dc3ce4c9903bb97ca6
Saddar U Din <saddarudin@nextgeni.com>
2026-05-19 12:38:10 +0500

    create employee view set with default crud operations

--- api/views.py
+++ api/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Company
-from .serializer import CompanySerializer
+from .models import Company, Employee
+from .serializer import CompanySerializer, EmployeeSerializer
 
 # viewsets provide the implementation for CRUD operations by default
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
