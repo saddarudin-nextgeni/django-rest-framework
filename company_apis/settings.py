ecae06a0f8e057e30549be72f7f3fb88dc1ccea8
Saddar U Din <saddarudin@nextgeni.com>
2026-05-19 14:24:52 +0500

    configured json rendering only and remove permission of unauthorized user

--- company_apis/settings.py
+++ company_apis/settings.py
@@ -53,7 +53,10 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
     ],
 }
 
