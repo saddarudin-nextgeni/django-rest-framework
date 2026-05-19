046a736622ef13439a8cb2701868fe09bab57f9c
Saddar U Din <saddarudin@nextgeni.com>
2026-05-19 12:52:07 +0500

    add __str__ method for employee

--- api/models.py
+++ api/models.py
@@ -20,3 +20,6 @@ class Employee(models.Model):
     phone = models.CharField(max_length=20)
     position = models.CharField(max_length=255)
     company = models.ForeignKey(Company, related_name='employees', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
